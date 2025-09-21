c9adae98b59edd889576f2717329a870716d5618
Kylan <kylansm98@yahoo.com>
2025-09-21 15:24:08 -0500

    Add mark task as completed function

--- Daily Habit Tracker/Task.py
+++ Daily Habit Tracker/Task.py
@@ -1,1 +1,10 @@
 def Task:
+    def__init(self, name, description, frequency, priority):
+        self.name = name
+        self.description = description
+        self.frequency = frequency
+        self.priority = priority
+        self.compleated = False
+
+    def mark_as_compleated(self):
+        self.compleated = True
